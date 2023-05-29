310d34b3b3bf49ffa798618ea2c3aa767578b26b
PavelVoitov <voitov.pavel@yandex.ru>
2023-05-29 17:11:29 +0300

    fix init messages

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import express from 'express'
 import http from  'http'
 import {Server} from 'socket.io'
+const cors = require('cors');
 
 const index = express()
 
+index.use(cors());
 
 const server = http.createServer(index)
 const socket = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000/"
+		origin: 'http://localhost:3000',
 	}
 });
+
+const messages = [
+		{message: "Hi", id: "sdfwaf5", user: {id: "4534132dawd", name: "Pavel Voitov"}},
+		{message: "Hi", id: "sdfdaf5", user: {id: "dwadwad5556", name: "chat GPT"}},
+	{message: "How are you?", id: "sdfddf5", user: {id: "dwadwad5556", name: "Pavel Voitov"}}
+	]
+
 
 const PORT = process.env.PORT || 3011
 
@@ -19,9 +28,11 @@ index.get('/', (req, res) => {
 });
 
 socket.on('connection', (socketChannel) => {
+	debugger
 	socketChannel.on('client-message-sent', (message: string) => {
 		console.log((message))
 	})
+	socketChannel.emit('init-messages-published', messages)
 })
 
 server.listen(PORT, () => {
