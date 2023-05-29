1b344647f5ee9a28de86c727e1a45315adf776c3
PavelVoitov <voitov.pavel@yandex.ru>
2023-05-29 18:22:30 +0300

    fix cors for socket

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ const server = http.createServer(index)
 const socket = new Server(server, {
 	cors: {
 		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true
 	}
 });
 
