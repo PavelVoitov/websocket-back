6288a6bf66c2004d5f34a23c259ec776e621140c
PavelVoitov <voitov.pavel@yandex.ru>
2023-05-29 12:30:37 +0300

    fix: app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const PORT = process.env.PORT || 3011
+
 app.get('/', (req, res) => {
 	res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3011, () => {
+server.listen(PORT, () => {
 	console.log('listening on *:3011');
 });
